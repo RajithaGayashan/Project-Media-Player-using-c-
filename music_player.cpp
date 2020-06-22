7aa62ee8bb9d139e84251fded17fa5ee76f04733
Rajitha Gayashan <40441007+RajithaGayashan@users.noreply.github.com>
2020-06-22 19:19:27 +0530

    Add files via upload
    updated

--- music_player.cpp
+++ music_player.cpp
@@ -10,7 +10,7 @@ void Player::MediaFunc(int x){
 				cout<<"\nPlaying..."<<endl;
 			}else{
 				if(pause){
-					cout<<"\nPlaying"<<endl;
+					cout<<"\nResuumed..."<<endl;
 					wsprintf(mediaCommand,"play %s",pathSrc);
 				}
 			}
@@ -20,19 +20,27 @@ void Player::MediaFunc(int x){
 			break;
 		
 		case 2:
+			if(!stop){
+				wsprintf(mediaCommand,"stop %s",pathSrc);
+			}
+			cout<<"\nStoped..."<<endl;
 			pause=false;
 			stop=true;
 			play=false;
 			break;
 			
-		cade 3:
+		case 3:
+			if(!pause){
+				wsprintf(mediaCommand,"pause %s",pathSrc);
+				cout<<"\nPaused...(a-resume/s>a-New file)\n"<<endl;
+			}
 			pause=true;
 			stop=false;
 			play=false;
 			break;
 	}
 	
-	if(msiSendString(mediaCommand,NULL,0,NULL)!=0){
+	if(mciSendString(mediaCommand,NULL,0,NULL)!=0){
 		system("cls");
 		MessageBox(NULL,"Sorry,can't paly your file","HALT!",0);
 		pause=false;
@@ -40,3 +48,21 @@ void Player::MediaFunc(int x){
 		play=false;
 	}
 }
+int  main(){
+	char c;
+	Player pl;
+	while(c!='f'){
+		cout<<"\nWhat you wanna do? \na-Play. \ns-Stop.\nd-Pause.\f-quit.\n"<<endl;
+		c=getch();
+		if(c=='a'){//play
+			pl.MediaFunc(1);
+		}else if(c=='s'){//stop
+			pl.MediaFunc(2);
+		}else if(c=='d'){//pause
+			pl.MediaFunc(3);
+		}
+	}
+	cout<<"\nBye!\n"<<endl;
+	exit(0);
+}
+//end;
